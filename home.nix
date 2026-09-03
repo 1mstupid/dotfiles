c33cda1276a105ca986dbd8606a8fd43aa182b9d
Ítalo Barros <italolv20@gmail.com>
2026-09-03 14:49:57 -0300

    minor

--- home.nix
+++ home.nix
@@ -144,9 +144,8 @@ in
 		# source = config.lib.file.mkOutOfStoreSymlink "/home/waltz/dotfiles/config/mango";
 		# recursive = true;
 		# };
-		"nvim" = {
-			source = config.lib.file.mkOutOfStoreSymlink "/home/waltz/dotfiles/config/nvim";
-			recursive = true;
+		"nvim/init.lua" = {
+			source = config.lib.file.mkOutOfStoreSymlink "/home/waltz/dotfiles/config/nvim/init.lua;
 		};
 		"hypr" = {
 			source = config.lib.file.mkOutOfStoreSymlink "/home/waltz/dotfiles/config/hypr";
