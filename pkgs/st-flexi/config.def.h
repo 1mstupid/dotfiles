0a312822d9bd0b988b6552da7cc0c68cccf64802
Ítalo Barros <italolv20@gmail.com>
2026-09-22 21:10:59 -0300

    minor

--- pkgs/st-flexi/config.def.h
+++ pkgs/st-flexi/config.def.h
@@ -226,7 +226,7 @@ static const char *colorname[] = {
 unsigned int defaultbg = 0.7;
 unsigned int bg = 17, bgUnfocused = 16;
 #else
-unsigned int defaultbg = 259;
+unsigned int defaultbg = 258;
 #endif // ALPHA_FOCUS_HIGHLIGHT_PATCH
 unsigned int defaultfg = 258;
 unsigned int defaultcs = 256;
