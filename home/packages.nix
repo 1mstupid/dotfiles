a2531ab1d57e5829fb7df945fedee6e25e53934c
Ítalo Barros <italolv20@gmail.com>
2026-09-22 21:54:38 -0300

    minor

--- home/packages.nix
+++ home/packages.nix
@@ -72,6 +72,7 @@ in
 		xdg-user-dirs
 		qt6Packages.sddm
 	  inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.ayugram-desktop.packages.${pkgs.system}.default
 		# mpvpaper 
 		# alacritty
 		# xinit
