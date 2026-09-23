736047c6ac0f11e4c1b737ef9516ef8a60ba64de
Ítalo Barros <italolv20@gmail.com>
2026-09-22 21:08:18 -0300

    minor

--- pkgs/st-flexi/config.def.h
+++ pkgs/st-flexi/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "CaskaydiaMono Nerd Font Mono:weight=900:pixelsize=15:antialias=true:autohint=true";
+static char *font = "CaskaydiaMono Nerd Font Mono:weight=900:pixelsize=18:antialias=true:autohint=true";
 
 #if FONT2_PATCH
 /* Spare fonts */
@@ -215,7 +215,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#000000", // cursor
 	"#000000",
-	"#1c1c1c", /* default foreground colour */
+	"#000000", /* default foreground colour */
 	"#000000", /* default background colour */
 };
 
