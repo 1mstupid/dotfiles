6fc3a3729d57407563ea7ee4a0a3cae12c3dd439
Ítalo Barros <italolv20@gmail.com>
2026-09-26 09:23:19 -0300

    minor

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -1,39 +1,4 @@
-{config, pkgs, ... }:
+{ config, pkgs, ... }:
 
-{
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      general = {
-        live_config_reload = true;
+throw "ALACRITTY MODULE IS LOADED"
 
-        import = [
-        ];
-      };
-
-      font = {
-        normal = {
-          family = "Caskaydia Nerd Font";
-          style = "Regular";
-        };
-
-        bold = {
-          family = "Caskaydia Nerd Font";
-          style = "Bold";
-        };
-
-        italic = {
-          family = "Caskaydia Nerd Font";
-          style = "Italic";
-        };
-
-        bold_italic = {
-          family = "Caskaydia Nerd Font";
-          style = "Bold Italic";
-        };
-
-        size = 13.0;
-      };
-    };
-  };
-}
