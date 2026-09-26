93d8f0b8cd3f405e440fff235d089be6eceda0a2
Ítalo Barros <italolv20@gmail.com>
2026-09-26 09:38:22 -0300

    minor

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -1,24 +1,69 @@
-{config, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.alacritty = {
     enable = true;
+
     settings = {
-      keyboard = {
-          bindings = [
-            {
-              chars = "\\u000c";
-              key = "K";
-              mods = "Control";
-            }
-          ];
-        };
-      window = {
-        dimensions = {
-            columns = 200;
-            lines = 3;
-        };
+      general = {
+        live_config_reload = true;
+
+        import = [
+          "${config.homeDirectory}/alacritty/current-theme.toml"
+        ];
       };
+
+      font = {
+        normal.family = "Caskaydia Nerd Font";
+        bold.family = "Caskaydia Nerd Font";
+        italic.family = "Caskaydia Nerd Font";
+        bold_italic.family = "Caskaydia Nerd Font";
+        size = 13.0;
+      };
+
+      selection.save_to_clipboard = true;
+
+      keyboard.bindings = [
+        {
+          key = "PageUp";
+          mods = "Control|Shift";
+          action = "IncreaseFontSize";
+        }
+        {
+          key = "PageDown";
+          mods = "Control|Shift";
+          action = "DecreaseFontSize";
+        }
+        {
+          key = "Home";
+          mods = "Control|Shift";
+          action = "ResetFontSize";
+        }
+        {
+          key = "C";
+          mods = "Control|Shift";
+          action = "Copy";
+        }
+        {
+          key = "V";
+          mods = "Control|Shift";
+          action = "Paste";
+        }
+        {
+          key = "PageUp";
+          mods = "Shift";
+          action = "ScrollPageUp";
+        }
+        {
+          key = "PageDown";
+          mods = "Shift";
+          action = "ScrollPageDown";
+        }
+        {
+          key = "F11";
+          action = "ToggleFullscreen";
+        }
+      ];
     };
   };
 }
--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -1,3 +1,3 @@
 {
-  imports = [ ./zsh.nix ./kitty.nix ];
+  imports = [ ./zsh.nix ./alacritty.nix ];
 }
