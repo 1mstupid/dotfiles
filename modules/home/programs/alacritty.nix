2bf57d9347ccb581f29f4f154c266ad8693c8c8c
Ítalo Barros <italolv20@gmail.com>
2026-09-26 09:21:22 -0300

    minor

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -4,21 +4,10 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window = {
-        padding = {
-          x = 30;
-          y = 20;
-        };
-
-        dynamic_padding = true;
-
-        decorations = "Full";
-      };
       general = {
         live_config_reload = true;
 
         import = [
-          "${config.home.homeDirectory}/.config/alacritty/current-theme.toml"
         ];
       };
 
@@ -45,65 +34,6 @@
 
         size = 13.0;
       };
-
-      selection = {
-        save_to_clipboard = true;
-      };
-
-      keyboard.bindings = [
-        # st: TERMMOD + Prior
-        {
-          key = "PageUp";
-          mods = "Control|Shift";
-          action = "IncreaseFontSize";
-        }
-
-        # st: TERMMOD + Next
-        {
-          key = "PageDown";
-          mods = "Control|Shift";
-          action = "DecreaseFontSize";
-        }
-
-        # st: TERMMOD + Home
-        {
-          key = "Home";
-          mods = "Control|Shift";
-          action = "ResetFontSize";
-        }
-
-        # st: TERMMOD + C / V
-        {
-          key = "C";
-          mods = "Control|Shift";
-          action = "Copy";
-        }
-
-        {
-          key = "V";
-          mods = "Control|Shift";
-          action = "Paste";
-        }
-
-        # st: Shift + PageUp / PageDown
-        {
-          key = "PageUp";
-          mods = "Shift";
-          action = "ScrollPageUp";
-        }
-
-        {
-          key = "PageDown";
-          mods = "Shift";
-          action = "ScrollPageDown";
-        }
-
-        # st: F11 fullscreen
-        {
-          key = "F11";
-          action = "ToggleFullscreen";
-        }
-      ];
     };
   };
 }
